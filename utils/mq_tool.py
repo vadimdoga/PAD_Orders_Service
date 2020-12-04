732d8303b7262fbf6cf703243116fad4b9942e20
Vadim <vadim.doga1@gmail.com>
2020-12-04 16:40:58 +0200

    added separate channel to each mq

--- utils/mq_tool.py
+++ utils/mq_tool.py
@@ -10,15 +10,14 @@ class MQTool:
     def __init__(self, queue_name):
         connection = pika.BlockingConnection(
             pika.ConnectionParameters(host='localhost'))
-        channel = connection.channel()
-        channel.queue_declare(queue=queue_name)
 
         self.queue_name = queue_name
-        self.channel = channel
         self.connection = connection
 
     def mq_publish(self, dict_body):
-        self.channel.basic_publish(
+        channel = self.connection.channel()
+        channel.queue_declare(queue=self.queue_name)
+        channel.basic_publish(
             exchange='', routing_key=self.queue_name, body=json.dumps(dict_body))
         logging.info(f"Message sent to queue: {self.queue_name}")
         self.connection.close()
@@ -28,8 +27,11 @@ class MQTool:
             logging.info(f"Message received on {self.queue_name}")
             update_final_order(data_body=body)
 
-        self.channel.basic_consume(
+        channel = self.connection.channel()
+        channel.queue_declare(queue=self.queue_name)
+
+        channel.basic_consume(
             queue=self.queue_name, on_message_callback=callback, auto_ack=True)
 
         logging.info(f"{self.queue_name} waiting for message!")
-        self.channel.start_consuming()
+        channel.start_consuming()

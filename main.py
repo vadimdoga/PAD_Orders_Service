e0b8cbe22037b71ba47a19407525c05fdbe77831
Vadim <vadim.doga1@gmail.com>
2020-12-04 12:22:02 +0200

    lunched events with threads

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from app.api.orders import Orders
 from app.flask_config import api, app
+from utils.mq_tool import MQTool
 
 import os
+import threading
 
-base_path = os.getenv("BASE_PATH")
+order_delivery_evt = MQTool(queue_name="ORDER_DELIVERY")
+compensation_order_created_evt = MQTool(queue_name="COMPENSATION_ORDER_CREATED")
 
-api.add_resource(Orders, base_path)
+api.add_resource(Orders, '/api/orders', '/api/orders/<string:order_id>')
 
 if __name__ == '__main__':
     HOST = os.getenv('HOST')
-    PORT = os.getenv('PORT')
+    PORT = int(os.getenv('PORT'))
+
+    # start thread events
+    final_order = threading.Thread(target=order_delivery_evt.mq_receive)
+    compensate_order = threading.Thread(target=compensation_order_created_evt.mq_receive)
+
+    final_order.start()
+    compensate_order.start()
+
     app.run(debug=True, host=HOST, port=PORT)
